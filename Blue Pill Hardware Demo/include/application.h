0e7d60c01d8b24cff51ce4c9f65b6dcf079ec4c3
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-02 12:27:45 -0400

    same

--- Blue Pill Hardware Demo/include/application.h
+++ Blue Pill Hardware Demo/include/application.h
@@ -13,6 +13,9 @@ namespace Application {
   const uint32_t I2S_DATA      = PA7;
   const uint32_t CAPTURE_RATIO = 3;
   const size_t   BLOCK_SIZE    = (Samples::NUM_ELEMENTS >> 2);
+  const uint32_t V_SPACING     = 48;
+
+
 
   uint16_t knob0;
   uint16_t knob1;
@@ -42,6 +45,117 @@ namespace Application {
   typedef lamb::oneshot_plus sample_t;
 
   sample_t * voices[4];
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  
+  void krate() {  
+    uint16_t tmp0 = knob0;
+    knob0 <<= 4;
+    knob0 -= tmp0;
+    knob0 += analogRead(PA0);
+    knob0 >>= 4;
+    pct = knob0 / 4092.0;
+  }
+
+  void graph() {
+  if (draw_flag) {
+    for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
+      if (voices[ix]->state) {
+        tft.fillRect(
+          36,
+          V_SPACING*ix+(V_SPACING >> 1),
+          V_SPACING-10,
+          V_SPACING-10,
+          ILI9341_RED
+        );
+      }
+      else {
+        tft.fillRect(
+          36,
+          V_SPACING*ix+(V_SPACING >> 1),
+          V_SPACING-10,
+          V_SPACING-10,
+          ILI9341_BLACK
+        );
+      }
+    }
+
+    draw_flag = false;
+  }            
+  
+  static uint16_t col = 0;
+  static const uint16_t col_max = 200; // real max 320
+  uint16_t tmp_col = col+120;
+  
+  tft.drawFastVLine(tmp_col, 0, 240, ILI9341_BLACK);
+  uint16_t tmp_knob0 = 119 - map(knob0, 0, 4091, 0, 119);
+  tft.drawPixel(tmp_col, tmp_knob0, ILI9341_GREEN);
+  tft.drawPixel(tmp_col, 239-tmp_knob0, ILI9341_GREEN);
+
+  int16_t tmp = drawbuff.read();
+  int16_t tmp_sample = map(tmp, -32768, 32767, -120, 119);
+  
+  if (tmp_sample > 0)
+    tft.drawFastVLine(
+      tmp_col,
+      120,
+      tmp_sample,
+      ILI9341_YELLOW
+    );
+  else 
+  if (tmp_sample < 0) {
+    tft.drawFastVLine(
+      tmp_col,
+      120 + tmp_sample,
+      abs(tmp_sample),
+      ILI9341_YELLOW);
+  }
+  
+  col++;
+  col %= col_max; 
+}
+  
+  void srate() {
+    if ((sample_ix % (1 << CAPTURE_RATIO)) == 0) {
+      avg_sample >>= CAPTURE_RATIO;
+
+      if (drawbuff.writable()) {
+        drawbuff.write(avg_sample);
+      }
+    
+      avg_sample = 0;
+    }
+  
+    int32_t sample = 0;
+  
+    for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
+      sample += voices[ix]->play();
+    }
+
+    sample >>= 1;
+
+    sample *= pct;
+  
+    avg_sample += sample;
+
+    pt8211.write_mono(sample);
+  
+    total_samples ++;
+    sample_ix ++;
+    sample_ix %= Samples::NUM_ELEMENTS; 
+  }
+
+  void draw_text() {
+    tft.fillRect(10, 10, 80, 80, ILI9341_BLACK);
+    tft.setCursor(10, 10);
+    tft.println(knob0);
+    tft.setCursor(10, 30);
+    tft.println(pct);
+    tft.setCursor(10, 50);
+    tft.println(knob1);
+  }
+
 }
 
 #endif
--- Blue Pill Hardware Demo/src/bp_demo.ino
+++ Blue Pill Hardware Demo/src/bp_demo.ino
@@ -4,9 +4,11 @@
 #include "samples.h"
 #include "application.h"
 
+using namespace Application;
+
 void setup() {
   for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
-    Application::voices[ix] = new Application::sample_t(Samples::data+Application::BLOCK_SIZE*ix, Application::BLOCK_SIZE);
+    voices[ix] = new sample_t(Samples::data+BLOCK_SIZE*ix, BLOCK_SIZE);
   }
   
 #ifdef ENABLE_SERIAL
@@ -15,43 +17,47 @@ void setup() {
   
   pinMode(PA0, INPUT);
  
-  Application::tft.begin();
-  Application::tft.setRotation(3);
-  Application::tft.setTextColor(ILI9341_WHITE);  
-  Application::tft.setTextSize(2);
-  Application::tft.fillScreen(ILI9341_BLACK);
+  tft.begin();
+  tft.setRotation(3);
+  tft.setTextColor(ILI9341_WHITE);  
+  tft.setTextSize(2);
+  tft.fillScreen(ILI9341_BLACK);
         
   SPI.begin();
 
-  Application::pt8211.begin(&SPI);
+  pt8211.begin(&SPI);
   
-  lamb::MapleTimer::setup(Application::timer_1, Application::SRATE, srate);
-  lamb::MapleTimer::setup(Application::timer_2, Application::KRATE, krate);
+  lamb::MapleTimer::setup(timer_1, SRATE, srate);
+  lamb::MapleTimer::setup(
+    timer_2,
+    KRATE,
+    krate
+  );
 
-  Application::timer_3.pause();
-  Application::timer_3.setPeriod(125000);
-  Application::timer_3.setChannel1Mode(TIMER_OUTPUT_COMPARE);
-  Application::timer_3.setCompare(TIMER_CH1, 0);
-  Application::timer_3.attachCompare1Interrupt(lrate);
-  Application::timer_3.refresh();    
-  Application::timer_3.resume();
+  timer_3.pause();
+  timer_3.setPeriod(125000);
+  timer_3.setChannel1Mode(TIMER_OUTPUT_COMPARE);
+  timer_3.setCompare(TIMER_CH1, 0);
+  timer_3.attachCompare1Interrupt(lrate);
+  timer_3.refresh();    
+  timer_3.resume();
 }
  
 void lrate() {
   for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
-    if (Tracks::data[Application::lrate_ix % Tracks::NUM_ELEMENTS][ix][0] > 0) {
-      Application::voices[ix]->trigger = true;
+    if (Tracks::data[lrate_ix % Tracks::NUM_ELEMENTS][ix][0] > 0) {
+      voices[ix]->trigger = true;
 
-      Application::voices[ix]->amplitude   = Tracks::data[
-        Application::lrate_ix % Tracks::NUM_ELEMENTS
+      voices[ix]->amplitude   = Tracks::data[
+        lrate_ix % Tracks::NUM_ELEMENTS
       ][ix][0];
       
-      Application::voices[ix]->phase_shift = Tracks::data[
-        Application::lrate_ix % Tracks::NUM_ELEMENTS
+      voices[ix]->phase_shift = Tracks::data[
+        lrate_ix % Tracks::NUM_ELEMENTS
       ][ix][1];
     }
 
-    Application::draw_flag = true;
+    draw_flag = true;
   }
   
   static uint32_t count = 0;
@@ -59,129 +65,20 @@ void lrate() {
 #ifdef ENABLE_SERIAL
   Serial.print(++count);
   Serial.print(": ");
-  Serial.print(Application::total_samples);
+  Serial.print(total_samples);
   Serial.print(" SRATE: ");
-  Serial.print(Application::SRATE);
+  Serial.print(SRATE);
   Serial.print(" KRATE: ");
-  Serial.print(Application::KRATE);
+  Serial.print(KRATE);
   Serial.println();
 #endif
 
-  Application::total_samples = 0;
-  Application::lrate_ix ++;
-}
-
-const uint32_t v_spacing = 48;
-
-void graph() {
-  if (Application::draw_flag) {
-    for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
-      if (Application::voices[ix]->state) {
-        Application::tft.fillRect(
-          36,
-          v_spacing*ix+(v_spacing >> 1),
-          v_spacing-10,
-          v_spacing-10,
-          ILI9341_RED
-        );
-      }
-      else {
-        Application::tft.fillRect(
-          36,
-          v_spacing*ix+(v_spacing >> 1),
-          v_spacing-10,
-          v_spacing-10,
-          ILI9341_BLACK
-        );
-      }
-    }
-
-    Application::draw_flag = false;
-  }            
-  
-  static uint16_t col = 0;
-  static const uint16_t col_max = 200; // real max 320
-  uint16_t tmp_col = col+120;
-  
-  Application::tft.drawFastVLine(tmp_col, 0, 240, ILI9341_BLACK);
-  uint16_t tmp_knob0 = 119 - map(Application::knob0, 0, 4091, 0, 119);
-  Application::tft.drawPixel(tmp_col, tmp_knob0, ILI9341_GREEN);
-  Application::tft.drawPixel(tmp_col, 239-tmp_knob0, ILI9341_GREEN);
-
-  int16_t tmp = Application::drawbuff.read();
-  int16_t tmp_sample = map(tmp, -32768, 32767, -120, 119);
-  
-  if (tmp_sample > 0)
-    Application::tft.drawFastVLine(
-      tmp_col,
-      120,
-      tmp_sample,
-      ILI9341_YELLOW
-    );
-  else 
-  if (tmp_sample < 0) {
-    Application::tft.drawFastVLine(
-      tmp_col,
-      120 + tmp_sample,
-      abs(tmp_sample),
-      ILI9341_YELLOW);
-  }
-  
-  col++;
-  col %= col_max; 
-}
-
-void krate() {  
-  uint16_t tmp0 = Application::knob0;
-  Application::knob0 <<= 4;
-  Application::knob0 -= tmp0;
-  Application::knob0 += analogRead(PA0);
-  Application::knob0 >>= 4;
-  Application::pct = Application::knob0 / 4092.0;
-}
-  
-void srate() {
-  if ((Application::sample_ix % (1 << Application::CAPTURE_RATIO)) == 0) {
-    Application::avg_sample >>= Application::CAPTURE_RATIO;
-
-    if (Application::drawbuff.writable()) {
-      Application::drawbuff.write(Application::avg_sample);
-    }
-    
-   Application::avg_sample = 0;
-  }
-  
-  int32_t sample = 0;
-  
-  for (size_t ix = 0; ix < Tracks::VOICE_COUNT; ix++) {
-    sample += Application::voices[ix]->play();
-  }
-
-  sample >>= 1;
-
-  sample *= Application::pct;
-  
-  Application::avg_sample += sample;
-
-  Application::pt8211.write_mono(sample);
-  
-  Application::total_samples ++;
-  Application::sample_ix ++;
-  Application::sample_ix %= Samples::NUM_ELEMENTS; 
-}
-
-void draw_text() {
-  Application::tft.fillRect(10, 10, 80, 80, ILI9341_BLACK);
-  Application::tft.setCursor(10, 10);
-  Application::tft.println(Application::knob0);
-  Application::tft.setCursor(10, 30);
-  Application::tft.println(Application::pct);
-  Application::tft.setCursor(10, 50);
-  Application::tft.println(Application::knob1);
+  total_samples = 0;
+  lrate_ix ++;
 }
 
 void loop(void) {
-  if (Application::drawbuff.readable()) {
+  if (drawbuff.readable()) {
     graph();
   }
 }
