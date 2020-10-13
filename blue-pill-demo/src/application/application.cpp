9479122de1dc3e272fd9e8cdc37c63b917f4eb5c
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-13 13:59:48 -0400

    names

--- blue-pill-demo/src/application/application.cpp
+++ blue-pill-demo/src/application/application.cpp
@@ -23,36 +23,88 @@ HardwareTimer                application::_timer_3             ( 3     );
 
 application::voice *         application::_voices              [ 6     ];
 application::draw_buffer     application::_draw_buffer;
-
+application::combined_source application::_combined_source;
 application::button          application::_button_device0;
 application::button          application::_button_device1;
 application::button          application::_button_device2;
 application::button          application::_button_device3;
 application::button          application::_button_device4;
 application::button          application::_button_device5;
-
 application::button_source   application::_button_source0(&_button_device0, 0);
 application::button_source   application::_button_source1(&_button_device1, 1);
 application::button_source   application::_button_source2(&_button_device2, 2);
 application::button_source   application::_button_source3(&_button_device3, 3);
 application::button_source   application::_button_source4(&_button_device4, 4);
 application::button_source   application::_button_source5(&_button_device5, 5);
+application::control_source  application::_control_event_source;
+application::dac             application::_dac(application::I2S_WS);
+application::tft            application::_tft(application::TFT_CS, application::TFT_DC);
 
-application::combined_source application::_combined_source;
-
-application::control_source
-application::_control_event_source;
-
-application::dac             application::_dac(
-  application::I2S_WS
-);
-
-application::tft             application::_tft(
-  application::TFT_CS,
-  application::TFT_DC
-); 
   
 //////////////////////////////////////////////////////////////////////////////
+
+void application::setup_controls() {
+  _button_device0      .setup(PB0);
+  _button_device1      .setup(PB1);
+  _button_device2      .setup(PB10);
+  _button_device3      .setup(PB11);
+  _button_device4      .setup(PC14);
+  _button_device5      .setup(PC15);
+  _combined_source     .sources[0] = &_button_source0;
+  _combined_source     .sources[1] = &_button_source1;
+  _combined_source     .sources[2] = &_button_source2;
+  _combined_source     .sources[3] = &_button_source3;
+  _combined_source     .sources[4] = &_button_source4;
+  _combined_source     .sources[5] = &_button_source5;
+  _control_event_source.source = &_combined_source;
+
+  pinMode(PA0, INPUT);
+  pinMode(PA1, INPUT);
+  pinMode(PA2, INPUT);  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void application::setup_voices() {
+  for (size_t ix = 0; ix < 6; ix++) {
+    _voices[ix] = new voice(Samples::data+BLOCK_SIZE*ix, BLOCK_SIZE);
+  }
+
+  _voices[0]->amplitude = 0xbf; // kick
+  _voices[1]->amplitude = 0xdf; // lo bass
+  _voices[2]->amplitude = 0xdf; // hi bass
+  _voices[3]->amplitude = 0x7f; // snare 
+  _voices[4]->amplitude = 0xff; // closed hat
+  _voices[5]->amplitude = 0x9f; // open hat
+}
+
+////////////////////////////////////////////////////////////////////////////////
+      
+void application::setup_tft() {
+  _tft.begin();
+  _tft.setRotation(3);
+  _tft.setTextColor(ILI9341_WHITE);  
+  _tft.setTextSize(2);
+  _tft.fillScreen(ILI9341_BLACK);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+void application::setup_dac() {
+  SPI.begin();
+    
+  _dac.begin(&SPI);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void application::setup_timers() {
+  maple_timer::setup(_timer_1, S_RATE, s_rate);
+  maple_timer::setup(_timer_2, K_RATE, k_rate);
+}
+
+////////////////////////////////////////////////////////////////////////////////
 
 application::application_event application::process_control_event(
   application::control_event const & control_event
@@ -77,7 +129,6 @@ application::application_event application::process_control_event(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
 application::application_event application::process_button_event(
   application::control_event const & control_event
 ) {
@@ -96,6 +147,42 @@ application::application_event application::process_button_event(
   application_event.parameter = button_number;
   
   return application_event;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void application::graph() {
+  static uint16_t col = 0;
+  static const uint16_t col_max = 200; // real max 320
+  uint16_t tmp_col = col+120;
+  
+  _tft.drawFastVLine(tmp_col, 0, 240, ILI9341_BLACK);
+  uint16_t tmp_knob0 = 119 - map(_knob0, 0, 4091, 0, 119);
+  _tft.drawPixel(tmp_col, tmp_knob0, ILI9341_GREEN);
+  _tft.drawPixel(tmp_col, 239-tmp_knob0, ILI9341_GREEN);
+
+  sample tmp = _draw_buffer.dequeue();
+
+  sample tmp_sample = map(tmp, -32768, 32767, -120, 119);
+  
+  if (tmp_sample > 0)
+    _tft.drawFastVLine(
+      tmp_col,
+      120,
+      tmp_sample,
+      ILI9341_YELLOW
+    );
+  else 
+    if (tmp_sample < 0) {
+      _tft.drawFastVLine(
+        tmp_col,
+        120 + tmp_sample,
+        abs(tmp_sample),
+        ILI9341_YELLOW);
+    }
+  
+  col ++;
+  col %= col_max; 
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -151,39 +238,7 @@ void application::k_rate() {
   _last_trigger_states = trigger_states;
 }
 
-void application::graph() {
-  static uint16_t col = 0;
-  static const uint16_t col_max = 200; // real max 320
-  uint16_t tmp_col = col+120;
-  
-  _tft.drawFastVLine(tmp_col, 0, 240, ILI9341_BLACK);
-  uint16_t tmp_knob0 = 119 - map(_knob0, 0, 4091, 0, 119);
-  _tft.drawPixel(tmp_col, tmp_knob0, ILI9341_GREEN);
-  _tft.drawPixel(tmp_col, 239-tmp_knob0, ILI9341_GREEN);
-
-  sample tmp = _draw_buffer.dequeue();
-
-  sample tmp_sample = map(tmp, -32768, 32767, -120, 119);
-  
-  if (tmp_sample > 0)
-    _tft.drawFastVLine(
-      tmp_col,
-      120,
-      tmp_sample,
-      ILI9341_YELLOW
-    );
-  else 
-    if (tmp_sample < 0) {
-      _tft.drawFastVLine(
-        tmp_col,
-        120 + tmp_sample,
-        abs(tmp_sample),
-        ILI9341_YELLOW);
-    }
-  
-  col++;
-  col %= col_max; 
-}
+////////////////////////////////////////////////////////////////////////////////
   
 void application::s_rate() {
   if ((_sample_ix % (1 << CAPTURE_RATIO)) == 0) {
@@ -208,69 +263,17 @@ void application::s_rate() {
     sample_ += tmp;
   }
 
-  sample_ >>= 1;
-
-  sample_ *= _master_vol;
-  
+  sample_    >>= 1;
+  sample_     *= _master_vol;
   _avg_sample += sample_;
 
   _dac.write_mono(sample_);
-  
-  _sample_ix ++;
-  _sample_ix %= Samples::NUM_ELEMENTS; 
+ 
+  _sample_ix  ++;
+  _sample_ix  %= Samples::NUM_ELEMENTS; 
 }
 
-void application::setup_controls() {
-  _button_device0      .setup(PB0);
-  _button_device1      .setup(PB1);
-  _button_device2      .setup(PB10);
-  _button_device3      .setup(PB11);
-  _button_device4      .setup(PC14);
-  _button_device5      .setup(PC15);
-  _combined_source     .sources[0] = &_button_source0;
-  _combined_source     .sources[1] = &_button_source1;
-  _combined_source     .sources[2] = &_button_source2;
-  _combined_source     .sources[3] = &_button_source3;
-  _combined_source     .sources[4] = &_button_source4;
-  _combined_source     .sources[5] = &_button_source5;
-  _control_event_source.source = &_combined_source;
-
-  pinMode(PA0, INPUT);
-  pinMode(PA1, INPUT);
-  pinMode(PA2, INPUT);  
-}
-
-void application::setup_voices() {
-  for (size_t ix = 0; ix < 6; ix++) {
-    _voices[ix] = new voice(Samples::data+BLOCK_SIZE*ix, BLOCK_SIZE);
-  }
-
-  _voices[0]->amplitude = 0xbf; // kick
-  _voices[1]->amplitude = 0xdf; // lo bass
-  _voices[2]->amplitude = 0xdf; // hi bass
-  _voices[3]->amplitude = 0x7f; // snare 
-  _voices[4]->amplitude = 0xff; // closed hat
-  _voices[5]->amplitude = 0x9f; // open hat
-}
-      
-void application::setup_tft() {
-  _tft.begin();
-  _tft.setRotation(3);
-  _tft.setTextColor(ILI9341_WHITE);  
-  _tft.setTextSize(2);
-  _tft.fillScreen(ILI9341_BLACK);
-}
-
-void application::setup_dac() {
-  SPI.begin();
-    
-  _dac.begin(&SPI);
-}
-
-void application::setup_timers() {
-  maple_timer::setup(_timer_1, S_RATE, s_rate);
-  maple_timer::setup(_timer_2, K_RATE, k_rate);
-}
+////////////////////////////////////////////////////////////////////////////////
 
 void application::setup() {
 #ifdef ENABLE_SERIAL
@@ -286,8 +289,17 @@ void application::setup() {
   delay(500);
 }
   
+////////////////////////////////////////////////////////////////////////////////
+
 void application::loop(void) {
   if (_draw_buffer.readable()) {
     graph();
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+/* Local Variables:  */
+/* fill-column: 100  */
+/* End:              */
