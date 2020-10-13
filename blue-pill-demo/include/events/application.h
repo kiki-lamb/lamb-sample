eb73ece0b0528832de8b62c7ed262e0907b174e2
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-10-12 22:04:49 -0400

    queues definitely working!

---
+++ blue-pill-demo/include/events/application.h
@@ -0,0 +1,26 @@
+#ifndef BP_DRUM_APPLICATION_EVENT_H
+#define BP_DRUM_APPLICATION_EVENT_H
+
+#include <Arduino.h>
+#include "lamb.h"
+
+namespace events {
+  enum application_event_type {
+    APP_EVT_NOT_AVAILABLE = 0,
+
+    EVT_TRIGGER = 1,
+    EVT_UNKNOWN = 64
+  };  
+
+  class application :
+    public lamb::events::event<application_event_type> {
+  public:
+    application(
+      event_type type_ = event_type::APP_EVT_NOT_AVAILABLE,
+      uint16_t arg = 0
+    ) :
+      event(type_, arg) {}
+  };
+}
+
+#endif
