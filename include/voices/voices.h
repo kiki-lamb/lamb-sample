305b2f617df4475861f8d9b425e787d0eca95bf7
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-03 19:11:42 -0500

    refactors

--- include/voices/voices.h
+++ include/voices/voices.h
@@ -14,46 +14,46 @@ public:
  typedef sample_q::value_type                                 sample;
  typedef lamb::oneshot<sample>                                voice;
  typedef typename lamb::sample_type_traits<sample>::mix_type  mix;
- typedef lamb::lowpass<sample>                                filter;
+ typedef lamb::lowpass                                        filter;
   
-  static const      uint32_t             S_RATE;
-  static const      uint8_t              COUNT              = 6;
-  static const      mix                  SILENCE            =
-    lamb::sample_type_traits<mix>::silence;
+ static const      uint32_t             S_RATE;
+ static const      uint8_t              COUNT              = 6;
+ static const      mix                  SILENCE            =
+  lamb::sample_type_traits<mix>::silence;
   
 private:
-  voices() = default;
+ voices() = default;
   
-  static constexpr  size_t               MAP[COUNT]         = { 0, 3, 5, 1, 1, 1 };
-  static const      uint8_t              MIDDLE_OCTAVE      = 4;
-  static const      uint8_t              ROOT_NOTE          = 46;
-  static const      uint8_t              BASS_ROOT_NOTE     = ((uint8_t)(ROOT_NOTE - 0));
-  static const      size_t               BLOCK_SIZE         =
-    Samples::NUM_ELEMENTS / COUNT;
+ static constexpr  size_t               MAP[COUNT]         = { 0, 3, 5, 1, 1, 1 };
+ static const      uint8_t              MIDDLE_OCTAVE      = 4;
+ static const      uint8_t              ROOT_NOTE          = 46;
+ static const      uint8_t              BASS_ROOT_NOTE     = ((uint8_t)(ROOT_NOTE - 0));
+ static const      size_t               BLOCK_SIZE         =
+  Samples::NUM_ELEMENTS / COUNT;
 
-  static            filter               _lpf;  
+ static            filter               _lpf;  
  static            lamb::u0q32::value_type        _phincrs[120];
  static            lamb::u0q16::value_type        _volume;
  static            lamb::u0q16::value_type        _scaled_volume;
-  static            voice *              _items[COUNT];
+ static            voice *              _items[COUNT];
   
-  static            void                 generate_phincrs();  
-  static            voice &              item(size_t const & ix);  
+ static            void                 generate_phincrs();  
+ static            voice &              item(size_t const & ix);  
   
 public:
-  static            void                 trigger(uint8_t const & ix);
+ static            void                 trigger(uint8_t const & ix);
  static            lamb::u0q16::value_type        volume();
-  static            void                 filter_f(lamb::u0q8::value_type const & f_);
-  static            void                 filter_q(lamb::u0q8::value_type const & q_);
+ static            void                 filter_f(lamb::u0q8::value_type const & f_);
+ static            void                 filter_q(lamb::u0q8::value_type const & q_);
  static            lamb::u0q8::value_type         filter_f();
  static            lamb::u0q8::value_type         filter_q();  
-  static            sample               read();
-  static            void                 setup();
-  static            bool                 volume(uint12_t const & volume);
-  static            bool                 pitch(
-    uint8_t  const & voice_ix,
-    uint12_t const & parameter
-  );
+ static            sample               read();
+ static            void                 setup();
+ static            bool                 volume(uint12_t const & volume);
+ static            bool                 pitch(
+  uint8_t  const & voice_ix,
+  uint12_t const & parameter
+ );
 };
 
 #endif
