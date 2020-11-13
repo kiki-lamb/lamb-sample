ec7752e43eb759a353bab8f2e54651686d9d31c8
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-12 23:08:47 -0500

    mess ith colors

--- include/application/application.h
+++ include/application/application.h
@@ -103,10 +103,10 @@ private:
    uint16_t x_pos = _x_pos + 12 * _name_len;
    
    application::_tft.setCursor(x_pos, _y_pos);
-   application::_tft.setTextColor(ILI9341_BLACK);
+   application::_tft.setTextColor( ((64 - _value) >> 1) << 5);
    application::_tft.setTextSize(2);
 
-   application::_tft.fillRect (x_pos, _y_pos - 1, _value, 17, ILI9341_GREEN);
+   application::_tft.fillRect (x_pos, _y_pos - 1, _value, 17, (_value >> 1) << 5);
    application::_tft.fillRect (x_pos + _value, _y_pos - 1, _width - _value, 17, ILI9341_BLACK);
 
    application::_tft.print(_value);
