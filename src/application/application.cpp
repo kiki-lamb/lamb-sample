845922a6ad40d9b656fb80cb0c96bbd207ace403
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-25 18:47:27 -0500

    WIP: Start ripping chunks out to make a minimal test/benchmark.

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -26,133 +26,6 @@ application::tft             application::_tft(
 
 application::draw_buffer     application::_draw_buffer;         
 
-application::displayed_value<voices::filter::unsigned_internal_t::value_type>
-application::_displayed_filter_freq { "Freq:", 184, 5,  10, 64 };
-
-application::displayed_value<voices::filter::unsigned_internal_t::value_type>
-application::_displayed_filter_res  { "Res: ", 184, 30, 10, 64 };
-
-application::displayed_value<u0q16::value_type>
-application::_displayed_vol         { "Vol: ", 184, 55, 10, 64 };
-
-////////////////////////////////////////////////////////////////////////////////
-
-bool application::graph() {
- if (_draw_buffer.count() < 16)
-  return false;
-
- voices::mix tmp { 0 };
- 
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- 
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- 
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- 
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- tmp  += _draw_buffer.dequeue();
- 
- tmp >>= 13; // to 7 bit
- 
- uint16_t wave_color = ILI9341_YELLOW;
- 
- if (tmp > 62) {
-  tmp.value = 62;
-  // wave_color = ILI9341_RED;
- }
- else if (tmp < -63) {
-  tmp.value = -63;
-  // wave_color = ILI9341_RED;
- }
- 
- static const uint16_t    width   = 172;
- static uint16_t          col     = 0;
- 
- uint16_t                 tmp_col = col % width;
- 
- static uint32_t          ctr     = 0;
- static uint32_t          accum   = 0;
- static constexpr uint8_t avging  = 12;
- 
- uint32_t                 start   = micros(); 
- 
- _tft.drawFastVLine(tmp_col, 1, 128 - 2, ILI9341_BLACK);
- 
- {
-  accum += micros() - start;
-  ctr   ++;
-  
-  if (ctr == (1 << avging)) {
-   accum >>= avging;
-   
-   Serial.print("t = ");
-   Serial.print(accum);
-   Serial.println();
-   
-   accum = 0;
-   ctr   = 0;
-  }
- }
- 
- if (tmp > 0)
-  _tft.drawFastVLine(
-   tmp_col,
-   64,
-   tmp.value,
-   wave_color
-  );
- else if (tmp < 0) {
-  _tft.drawFastVLine(
-   tmp_col,
-   64 + tmp.value,
-   abs(tmp.value),
-   wave_color
-  );
- }
-
- _tft.drawFastHLine(0,      64,  width, ILI9341_RED);
- 
- static bool box = false;
- 
- if (! box) {
-  _tft.drawFastHLine(0,     128, width, ILI9341_GREEN);
-  _tft.drawFastVLine(width, 0,   128,   ILI9341_GREEN);
-  
-  box = true;  
- }
- 
- col ++;
- 
- static uint32_t last_time = 0;
- uint32_t         new_time = millis();
- 
- if ((new_time - last_time) > 500) {
-  _tft.setCursor(10, 210);
-  _tft.fillRect(10, 210 - 2, 100, 20, ILI9341_BLACK);
-  _tft.setTextColor(ILI9341_GREEN);
-  _tft.setTextSize(2);
-  _tft.print(new_time);
-  
-  // Serial.print("Time: ");
-  // Serial.println(new_time);
-  
-  last_time = new_time;
- }
-
- return true;
-}
-
 //////////////////////////////////////////////////////////////////////////////
 
 void application::k_rate() {
@@ -381,8 +254,6 @@ void application::setup() {
  setup_sd();
 
  setup_tft();
-
- update_displayed_values();
  
  setup_dac();
  
@@ -396,68 +267,44 @@ void application::setup() {
   
 ////////////////////////////////////////////////////////////////////////////////
 
-void application::update_displayed_values() {
- _displayed_filter_freq.update(voices::filter_f().value);
- _displayed_filter_res .update(voices::filter_q().value);
- _displayed_vol        .update(voices::volume().value);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 void application::loop() {
- static u16q16   draw_operations                  { 0 };
-
- static uint32_t last_params_draw                 = 0;
- static uint32_t last_auto_trigger                = 0;
-
- uint32_t now = millis();
-
- if ((now - last_params_draw) > 100) {
-  last_params_draw = now;
-
-  update_displayed_values();
- } 
+ uint16_t wave_color = ILI9341_YELLOW;
  
-#ifdef ENABLE_SD
- File root { SD.open("/") };
+ static const uint16_t    width   = 240;
+ static uint16_t          col     = 0; 
+
+ uint16_t                 tmp_col = col % width;
  
- print_directory(root);
-
- root.close();
-#endif
+ static uint32_t          ctr     = 0;
+ static uint32_t          accum   = 0;
+ static constexpr uint8_t avging  = 12;
  
- if ((now - last_auto_trigger) > 500) {
-  last_auto_trigger = now;
-
-#ifdef TEST_TRIGGER
-  voices::trigger(0);
-#endif
- } 
-
- if (graph())
-  draw_operations += u16q16(1, 0);
-
-#ifdef LOG_DRAW_RATES
- if (_sample_ix1 >= voices::S_RATE) {
-  static u16q16        avg_draw_operations       { 0 };
-  static uint32_t      tenth_seconds             = 0;
-  tenth_seconds                                 += 1;
-  _sample_ix1                                    = 0;
-  avg_draw_operations                           *= u16q16(0xe000);
-  avg_draw_operations                           += draw_operations * u16q16(0x1fff);
-    
-  Serial.print(tenth_seconds);
-  Serial.print(F(", "));
-  Serial.print(float(draw_operations));
-  Serial.print(F(", "));
-  Serial.print(float(avg_draw_operations));
-  Serial.print(F(" avg, "));
-  Serial.print(_draw_buffer.count());
+ uint32_t                 start   = micros(); 
+ 
+ _tft.drawFastVLine(tmp_col, 1, 128 - 2, ILI9341_BLACK);
+ 
+ accum += micros() - start;
+ ctr   ++;
+ 
+ if (ctr == (1 << avging)) {
+  accum >>= avging;
+  
+  Serial.print("t = ");
+  Serial.print(accum);
   Serial.println();
-    
-  draw_operations.value                          = 0;
+  
+  accum = 0;
+  ctr   = 0;
  }
-#endif
+ 
+ _tft.drawFastVLine(
+  tmp_col,
+  0,
+  tmp_col,
+  wave_color
+ );
+ 
+ col ++; 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
