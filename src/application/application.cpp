20ae8f9a760a63869f9d52ed128af6b7d1637fb3
Kiki Lamb <miss.kiki.lamb@gmail.com>
2020-11-25 21:33:00 -0500

    Simplify more.

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -3,13 +3,11 @@
 
 using namespace lamb;
 
-using namespace lamb::tables;
-
 //////////////////////////////////////////////////////////////////////////////
 
-SPIClass                     application::spi_1    { 1        };
-HardwareTimer                application::tim_1  { 1        };
-Adafruit_ILI9341_STM         application::tft      { PA4, PB0 };
+SPIClass             application::spi_1 { 1        };
+HardwareTimer        application::tim_1 { 1        };
+Adafruit_ILI9341_STM application::tft   { PA4, PB0 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -25,8 +23,6 @@ void application::setup() {
 
  tft.begin(spi_1);
  tft.setRotation(3);
- tft.setTextColor(ILI9341_WHITE);  
- tft.setTextSize(2);
  tft.fillScreen(ILI9341_BLACK);
 
  device::maple_timer::setup(tim_1, 48000, sample_rate);
@@ -35,42 +31,37 @@ void application::setup() {
 ////////////////////////////////////////////////////////////////////////////////
 
 void application::loop() {
- static const uint16_t    draw_area_width = 240;
- static uint16_t          column          = 0; 
+ constexpr uint16_t draw_area_width = 240;
+ static uint16_t    column          = 0; 
 
- uint16_t                 tmp_col         = column % draw_area_width;
- 
- constexpr uint8_t        fx_shift        = 8;
- constexpr size_t         max_count       = 1 << fx_shift;
- static size_t            counter         = 0;
- static uint32_t          accum           = 0;
- 
- uint32_t                 start           = micros(); 
- 
- tft.drawFastVLine(column % draw_area_width, 1, 128 - 2, ILI9341_BLACK);
- 
- accum   += micros() - start;
- counter ++;
- 
- if (counter == (1 << fx_shift)) {
-  accum >>= fx_shift;
-  
-  Serial.print("t = ");
-  Serial.print(accum);
-  Serial.println();
-  
-  accum = 0;
-  counter   = 0;
- }
+ // We're going to take the average time every 256 writes:
+ constexpr uint8_t  fx_shift        = 8;
+ constexpr size_t   max_count       = 1 << fx_shift;
+ static size_t      counter         = 0;
+ static uint32_t    accum           = 0;
+
+ uint32_t           start           = micros(); 
  
  tft.drawFastVLine(
   column % draw_area_width,
   0,
-  column % draw_area_width,
+  column++ % draw_area_width,
   ILI9341_YELLOW
  );
+
+ accum   += micros() - start;
+ counter ++;
  
- column ++; 
+ if (counter == max_count) {
+  accum >>= fx_shift;
+  
+  Serial.print(accum);
+  Serial.println();
+  
+  accum   = 0;
+  counter = 0;
+ }
+ 
 }
 
 ////////////////////////////////////////////////////////////////////////////////
